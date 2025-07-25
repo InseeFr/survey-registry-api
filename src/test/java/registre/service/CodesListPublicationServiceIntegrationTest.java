4797f5b4fe0a2b0b31b442a8bd978ec324f6d846
Verriez Rémi <remi.verriez@insee.fr>
2025-07-25 11:00:58 +0200

    test: add integration test for recovery codeslist service

--- src/test/java/registre/service/CodesListPublicationServiceIntegrationTest.java
+++ src/test/java/registre/service/CodesListPublicationServiceIntegrationTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class CodesListPublicationServiceIT {
+class CodesListPublicationServiceIntegrationTest {
 
     @Autowired
     private CodesListPublicationService service;
