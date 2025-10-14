3b0001af14532bb47cbcb8e056dafb68a0605a9d
RemiVerriez <remi.verriez@insee.fr>
2025-10-14 10:10:07 +0200

    feat: add not blank properties in codeslistdto

--- src/main/java/registre/dto/CodesListDto.java
+++ src/main/java/registre/dto/CodesListDto.java
@@ -2,6 +2,8 @@ package registre.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
@@ -16,6 +18,8 @@ public record CodesListDto(
         @JsonProperty("id")
         UUID id,
 
+        @Valid
+        @NotNull
         @Schema(
                 name = "metadata",
                 requiredMode = Schema.RequiredMode.REQUIRED
